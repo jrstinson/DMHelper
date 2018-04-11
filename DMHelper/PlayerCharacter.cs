9b34f86dea1eb9be13c13f731070bc99c11aeaab
Jacob Stinson <jstinson14@outlook.com>
2018-04-10 22:31:13 -0400

    First commit

---
+++ DMHelper/PlayerCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMHelper
+{
+    class PlayerCharacter
+    {
+        protected int hitpoints;
+        protected int str;
+        protected int con;
+        protected int cha;
+        protected int 
+    }
+}
